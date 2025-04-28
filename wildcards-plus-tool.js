859076448091dba83f34d1cc6fb9064a5574ea86
Ariane Emory <ariane.emory@protonmail.com>
2025-04-28 14:39:49 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -39,8 +39,7 @@ import { stdin as input, stdout as output } from 'process';
 // helper functions:
 // ---------------------------------------------------------------------------------------
 function post_prompt(prompt, hostname = '127.0.0.1', port = 7860) {
-  console.log("FAKE POST");
-  return;
+  console.log("POSTing!");
   
   const data = JSON.stringify({
     prompt: prompt,
