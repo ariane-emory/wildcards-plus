3cdf1cd923dba7ef5ff45c4fdd4f315b7abfc2d7
Ariane Emory <ariane.emory@protonmail.com>
2025-04-30 17:41:19 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -1733,24 +1733,25 @@ function make_context(flags = new Set(),
 // ---------------------------------------------------------------------------------------
 function load_prelude(into_context = make_context()) {
   const prelude = `
-    @pro_3rd_subj       := {?female she |?male he  |?neuter it  | !female !male !neuter {3 #female|2 #male|#neuter} @pro_3rd_subj}
-    @pro_3rd_obj        := {?female her |?male him |?neuter it  | !female !male !neuter {3 #female|2 #male|#neuter} @pro_3rd_obj }
-    @pro_pos_adj        := {?female her |?male his |?neuter its | !female !male !neuter {3 #female|2 #male|#neuter} @pro_pos_adj }
-    @pro_pos            := {?female hers|?male his |?neuter its | !female !male !neuter {3 #female|2 #male|#neuter} @pro_pos     }
-    @__digit            := {<0|<1|<2|<3|<4|<5|<6|<7|<8|<9}
-    @__high_digit       := {<5|<6|<7|<8|<9}
-    @random_weight      := {:1. @__digit}
-    @high_random_weight := {:1. @__high_digit}
+    @set_gender_if_unset := {!female !male !neuter {3 #female|2 #male|#neuter}}
+    @pro_3rd_subj        := {@set_gender_if_unset {?female she |?male he  |?neuter it }}
+    @pro_3rd_obj         := {@set_gender_if_unset {?female her |?male him |?neuter it }}
+    @pro_pos_adj         := {@set_gender_if_unset {?female her |?male his |?neuter its}}
+    @pro_pos             := {@set_gender_if_unset {?female hers|?male his |?neuter its}}
+    @__digit             := {<0|<1|<2|<3|<4|<5|<6|<7|<8|<9}
+    @__high_digit        := {<5|<6|<7|<8|<9}
+    @random_weight       := {:1. @__digit}
+    @high_random_weight  := {:1. @__high_digit}
 
-    @pony_score_9       := {score_9}
-    @pony_score_8_up    := {score_9, score_8_up}
-    @pony_score_7_up    := {score_9, score_8_up, score_7_up}
-    @pony_score_6_up    := {score_9, score_8_up, score_7_up, score_6_up}
-    @pony_score_5_up    := {score_9, score_8_up, score_7_up, score_6_up, score_5_up}
-    @pony_score_4_up    := {score_9, score_8_up, score_7_up, score_6_up, score_5_up, score_4_up}
-    @aris_defaults      := {masterpiece, best quality, absurdres, aesthetic, 8k,
-                            focused subject, ultra high resolution, detailed background,
-                            wide shot}
+    @pony_score_9        := {score_9}
+    @pony_score_8_up     := {score_9, score_8_up}
+    @pony_score_7_up     := {score_9, score_8_up, score_7_up}
+    @pony_score_6_up     := {score_9, score_8_up, score_7_up, score_6_up}
+    @pony_score_5_up     := {score_9, score_8_up, score_7_up, score_6_up, score_5_up}
+    @pony_score_4_up     := {score_9, score_8_up, score_7_up, score_6_up, score_5_up, score_4_up}
+    @aris_defaults       := {masterpiece, best quality, absurdres, aesthetic, 8k,
+                             focused subject, ultra high resolution, detailed background,
+                             wide shot}
 
     // Integrated 'Wizard's Large Scroll of Artist Summoning':
     @wizards_artists := {
