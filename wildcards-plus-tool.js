111101601ccfb9fdbfe745761a87e8e54b2b1857
Ariane Emory <ariane.emory@protonmail.com>
2025-05-01 03:36:20 -0400

    mysteriously fixed

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -3948,8 +3948,7 @@ function expand_wildcards(thing, context = make_context()) {
     // Flags:
     // -----------------------------------------------------------------------------------
     else if (thing instanceof ASTSetFlag) {
-      if (context.noisy)
-        console.log(`SET FLAG '${thing.name}'.`);
+      console.log(`SET FLAG '${thing.name}'.`);
       
       context.flags.add(thing.name);
 
@@ -4088,16 +4087,17 @@ function expand_wildcards(thing, context = make_context()) {
       for (const option of thing.options) {
         let skip = false;
 
-        // console.log(`alternative is guarded against ${inspect_fun(option.not_flags.map(nf => nf.name).join(", "))}`);
+        if (option.not_flags.length > 1)
+          console.log(`alternative ${inspect_fun(option.body).replace(/\s+/, ' ')} is guarded against ${inspect_fun(option.not_flags.map(nf => nf.name).join(", "))}`);
         
         for (const not_flag of option.not_flags) {
-          if (context.noisy) 
-            // console.log(`CHECKING FOR NOT ${inspect_fun(not_flag.name)}...`);
+          console.log(`CHECKING FOR NOT ${inspect_fun(not_flag.name)} in ${inspect_fun(Array.from(context.flags))}...`);
 
-            if (context.flags.has(not_flag.name)) {
-              skip = true;
-              break;
-            }
+          if (context.flags.has(not_flag.name)) {
+            console.log(`FOUND ${inspect_fun(not_flag.name)} in ${inspect_fun(Array.from(context.flags))}, forbid!`);
+            skip = true;
+            break;
+          }
         }
 
         if (skip)
@@ -4110,10 +4110,10 @@ function expand_wildcards(thing, context = make_context()) {
           let dont_skip = false;
           
           for (const name of check_flag.names) {
-            // console.log(`check for ${name} in ${inspect_fun(Array.from(context.flags))}: ${context.flags.has(name)} during ${inspect_fun(option.body)}`);
+            console.log(`check for ${name} in ${inspect_fun(Array.from(context.flags))}: ${context.flags.has(name)} during ${inspect_fun(option.body)}`);
             
             if (context.flags.has(name)) {
-              // console.log(`found ${name} in ${inspect_fun(Array.from(context.flags))}.`);
+              console.log(`FOUND ${name} in ${inspect_fun(Array.from(context.flags))}, allow!`);
               dont_skip = true;
               break;
             }
