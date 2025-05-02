25cf3f0c11e3b07ccdcbe5c35f9b998ae189281f
Ariane Emory <ariane.emory@protonmail.com>
2025-05-02 17:41:04 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -4895,7 +4895,7 @@ async function main() {
 
   console.log('--------------------------------------------------------------------------------');
 
-  const json_str = `[false, {"foo": 123, "bar": 456}]`;
+  const json_str = `[false, null, {"foo": 123, "bar": 456}]`;
   console.log(inspect_fun(json.match(json_str)));
   console.log(JSON.stringify(json.match(json_str).value));
 }
