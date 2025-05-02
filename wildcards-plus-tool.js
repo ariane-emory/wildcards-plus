01f8b57377a0b5e3f28df9aa2b8bb1bae6a1413c
Ariane Emory <ariane.emory@protonmail.com>
2025-05-02 12:59:49 -0400

    test json

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -4809,6 +4809,10 @@ async function main() {
   }
 
   console.log('--------------------------------------------------------------------------------');
+
+  const json_str = `{ "foo": 123, "bar": 456}`;
+  console.log(inspect_fun(json.match(json_str)));
+  console.log(JSON.stringify(json.match(json_str).value));
 }
 
 // ---------------------------------------------------------------------------------------
