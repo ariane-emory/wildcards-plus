17e9f63678f3621e61b4ab20220c41f316866c54
Ariane Emory <ariane.emory@protonmail.com>
2025-05-02 16:14:40 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -367,7 +367,13 @@ class Quantified extends Rule {
     let match_result = this.rule.match(
       input, index, indent + 1);
 
-    if (! match_result)
+    if (match_result === undefined)
+      throw new Error("left");
+    
+    if (match_result === false)
+      throw new Error("right");
+    
+    if (match_result === null)
       return new MatchResult([], input, index);
 
     if (match_result.value === '' || match_result.value)
