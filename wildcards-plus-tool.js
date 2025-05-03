e19e7c5746492402e17bfba4ecafd146448eb0d8
Ariane Emory <ariane.emory@protonmail.com>
2025-05-02 20:14:21 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -4675,7 +4675,8 @@ const tld_fun = arr => new ASTSpecialFunction(...arr);
 // other non-terminals:
 const SpecialFunctionName     = choice('include', 'fake'); // choice('include', 'models');
 const SpecialFunction         = xform(tld_fun,
-                                      c_funcall(second(seq('%', SpecialFunctionName)), json));
+                                      c_funcall(second(seq('%', SpecialFunctionName)),
+                                                json));
 const AnonWildcardAlternative      = xform(make_ASTAnonWildcardAlternative,
                                            seq(wst_star(choice(comment, TestFlag, SetFlag)),
                                                optional(wb_uint, 1),
