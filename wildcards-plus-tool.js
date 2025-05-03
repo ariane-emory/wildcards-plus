70401a0ef729ca3e92316c22796beab964ab02d4
Ariane Emory <ariane.emory@protonmail.com>
2025-05-03 16:42:56 -0400

    wip

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -4862,11 +4862,13 @@ const SFUpdateConfigurationUnary   = xform(wst_seq('%config',            // 0
                                                    ')'),                 // 3
                                            arr => new ASTSpecialFunction('update-config',
                                                                          [arr[2]]));
-const SFSetConfiguration           = xform(wst_seq('%config', DiscardedComments,
-                                                   assignment_operator,
-                                                   DiscardedComments,jsonc_object),
-                                           arr => new ASTSpecialFunction('set-config',
-                                                                         [arr[1]]));
+const SFSetConfiguration           = xform(wst_seq('%config',            // 0
+                                                   DiscardedComments,    // -
+                                                   assignment_operator,  // 1
+                                                   DiscardedComments,    // -
+                                                   jsonc_object),        // 2
+                                           arr => { return new ASTSpecialFunction('set-config',
+                                                                                  [arr[1]]); });
 const SFUpdateConfiguration        = choice(SFUpdateConfigurationUnary,
                                             SFUpdateConfigurationBinary);
 const SpecialFunction              = choice(dt_hosted? never_match : SFInclude,
