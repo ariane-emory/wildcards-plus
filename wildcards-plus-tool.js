fdc476de2a3899a52ab47b314ef7b49e74a8827d
Ariane Emory <ariane.emory@protonmail.com>
2025-05-03 14:48:46 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -2080,9 +2080,9 @@ function munge_config(config, is_dt_hosted = dt_hosted) {
   config = { ...config };
   
   if (is_dt_hosted) { // running in DT, sampler needs to be an index:
-    if (config.sampler && typeof config.sampler === 'string') {
+    if (config.sampler !== undefined && typeof config.sampler === 'string') {
       console.log(`Correcting config.sampler = ${config.sampler} to ` +
-                  `${dt_samplers.indexOf(config.sampler)}.`);
+                  `config.${dt_samplers.indexOf(config.sampler)}.`);
       config.sapler = dt_samplers.indexOf(config.sampler);
     }
 
@@ -2090,16 +2090,16 @@ function munge_config(config, is_dt_hosted = dt_hosted) {
       if (config[automatic1111_name] !== undefined) {
         console.log(`Correcting config.${automatic1111_name} = ` +
                     `${config[automatic1111_name]} to ` +
-                    `config.${dt_name} = ${config[automatic1111_name]}.`);
+                    `config.sampler = ${config[automatic1111_name]}.`);
         config[dt_name] = config[automatic1111_name];
         delete config[automatic1111_name];
       }
     }
   }
   else { // running in Node.js, sampler needs to be a string:
-    if (config.sampler && typeof config.sampler ===  'number') {
+    if (config.sampler !== undefined && typeof config.sampler ===  'number') {
       console.log(`Correcting config.sampler = ${config.sampler} to ` +
-                  `${inspect_fun(dt_samplers[config.sampler])}.`);
+                  `config.sampler = ${inspect_fun(dt_samplers[config.sampler])}.`);
       config.sampler = dt_samplers[config.sampler];
     }
 
