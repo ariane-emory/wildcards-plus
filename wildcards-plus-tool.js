914ac9d3f6487ac3fa94a524e1cf40513a50552b
Ariane Emory <ariane.emory@protonmail.com>
2025-05-08 21:04:17 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -4936,48 +4936,48 @@ function expand_wildcards(thing, context = new Context()) {
       return '';
     }
     // -----------------------------------------------------------------------------------
-    else if (thing instanceof ASTSpecialFunction && thing.directive == 'update-config') {
-      if (thing.args.length > 2)
-        throw new Error(`update-config takes 1 or 2 arguments, got ` +
-                        `${inspect_fun(thing.args)}`);
+    // else if (thing instanceof ASTSpecialFunction && thing.directive == 'update-config') {
+    //   if (thing.args.length > 2)
+    //     throw new Error(`update-config takes 1 or 2 arguments, got ` +
+    //                     `${inspect_fun(thing.args)}`);
 
-      let config = {};
+    //   let config = {};
 
-      if (thing.args.length === 2) {
-        // TOOD: maybe check types? unsure.
-        config[thing.args[0]] = thing.args[1];
-      }
-      else {
-        config = thing.args[0];
-      }
-      
-      if (typeof config !== 'object')
-        throw new Error(`update-config's argument must be either: an object OR a ` +
-                        `string and an object, got ${inspect_fun(config)}`);
+    //   if (thing.args.length === 2) {
+    //     // TOOD: maybe check types? unsure.
+    //     config[thing.args[0]] = thing.args[1];
+    //   }
+    //   else {
+    //     config = thing.args[0];
+    //   }
+    
+    //   if (typeof config !== 'object')
+    //     throw new Error(`update-config's argument must be either: an object OR a ` +
+    //                     `string and an object, got ${inspect_fun(config)}`);
 
-      context.config = { ...context.config, ...config };
+    //   context.config = { ...context.config, ...config };
 
-      if (log_config_enabled)
-        console.log(`Updated config to ${JSON.stringify(context.config)}`);
-      
-      return '';
-    } 
-    if (thing instanceof ASTSpecialFunction && thing.directive == 'set-config') {
-      const config = thing.args[0];
-      
-      context.add_loras = []; // kinda ugly but seems correct for this case...
+    //   if (log_config_enabled)
+    //     console.log(`Updated config to ${JSON.stringify(context.config)}`);
+    
+    //   return '';
+    // } 
+    // if (thing instanceof ASTSpecialFunction && thing.directive == 'set-config') {
+    //   const config = thing.args[0];
+    
+    //   context.add_loras = []; // kinda ugly but seems correct for this case...
 
-      if (typeof config !== 'object')
-        throw new Error(`set-config's argument must be an object, ` +
-                        `got ${inspect_fun(config)}`);
+    //   if (typeof config !== 'object')
+    //     throw new Error(`set-config's argument must be an object, ` +
+    //                     `got ${inspect_fun(config)}`);
 
-      context.config = config;
+    //   context.config = config;
 
-      if (log_config_enabled)
-        console.log(`Set config to ${JSON.stringify(config)}`);
-      
-      return '';
-    } 
+    //   if (log_config_enabled)
+    //     console.log(`Set config to ${JSON.stringify(config)}`);
+    
+    //   return '';
+    // } 
     else if (thing instanceof ASTSpecialFunction) {
       // console.log(`IGNORING ${inspect_fun(thing)}`);
       console.log(`IGNORING UNIMPLEMENTED SpecialFunction: ${JSON.stringify(thing)}`);
