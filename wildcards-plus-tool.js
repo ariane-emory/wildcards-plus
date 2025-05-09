d7b21ed5078f89e3875f22bef6d10626aab71b2a
Ariane Emory <ariane.emory@protonmail.com>
2025-05-09 02:32:20 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -5428,21 +5428,8 @@ const tld_fun = arr => new ASTSpecialFunction(...arr);
 // other non-terminals:
 // ---------------------------------------------------------------------------------------
 const DiscardedComments             = discard(wst_star(comment));
-const SpecialFunctionInclude                     = make_special_function_Rule('include');
-let SpecialFunctionUpdateConfigurationBinary   =
-    xform(wst_cutting_seq(wst_seq('%config',             // [0][0]
-                                  DiscardedComments,     // -
-                                  '.',                   // [0][1]
-                                  DiscardedComments),    // -
-                          ident,                         // [1]
-                          DiscardedComments,             // -
-                          '(',                           // [2]
-                          DiscardedComments,             // -
-                          Jsonc,   // [3]
-                          DiscardedComments,             // [4]
-                          ')'),                          // [4]
-          arr => new ASTSpecialFunctionUpdateConfigBinary(arr[1], arr[3]));
-SpecialFunctionUpdateConfigurationBinary =
+const SpecialFunctionInclude                 = make_special_function_Rule('include');
+let   SpecialFunctionUpdateConfigurationBinary =
   xform(wst_cutting_seq(wst_seq('%config',             // [0][0]
                                 DiscardedComments,     // -
                                 '.',                   // [0][1]
