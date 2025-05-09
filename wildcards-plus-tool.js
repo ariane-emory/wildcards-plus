bf752c5a2bedbf016ad12dcd47c4c0b255654c2f
Ariane Emory <ariane.emory@protonmail.com>
2025-05-09 03:14:14 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -5362,7 +5362,7 @@ class ASTSpecialFunctionSetPickMultiple extends AST {
 // ---------------------------------------------------------------------------------------
 const word_break              = /(?=\s|[{|}]|$)/;
 const plaintext               = /[^{|}\s]+/;
-const plaintext_no_rpar       = /[^{|}\s\)]+/;
+// const plaintext_no_rpar       = /[^{|}\s\)]+/;
 const low_pri_text            = /[\(\)\[\]\,\.\?\!\:\;]+/;
 const wb_uint                 = xform(parseInt, /\b\d+(?=\s|[{|}]|$)/);
 const ident                   = /[a-zA-Z_-][0-9a-zA-Z_-]*\b/;
