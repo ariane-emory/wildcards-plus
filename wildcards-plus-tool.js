19d8a797fe9789f8b4347b782f453ae7aeb32a09
Ariane Emory <ariane.emory@protonmail.com>
2025-05-09 05:18:54 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -5299,20 +5299,6 @@ const unarySpecialFunction = (prefix, rule, xform_func) =>
               return xform_func(arr[1]);
             });
 // -------------------------------------------------------------------------------------------------
-// A1111-style LoRAs:
-// -------------------------------------------------------------------------------------------------
-const A1111StyleLoraWeight = choice(/\d*\.\d+/, /\d+/);
-const A1111StyleLora       = xform(arr => new ASTLora(arr[3], arr[4][0]),
-                                   wst_seq('<',                                    // [0]
-                                           'lora',                                 // [1]
-                                           ':',                                    // [2]
-                                           choice(filename, () => LimitedContent), // [3]
-                                           optional(second(wst_seq(':',
-                                                                   choice(A1111StyleLoraWeight,
-                                                                          () => LimitedContent))),
-                                                    "1.0"), // [4][0]
-                                           '>'));
-// -------------------------------------------------------------------------------------------------
 // helper funs used by xforms:
 // -------------------------------------------------------------------------------------------------
 const make__ASTAnonWildcardAlternative = arr => {
@@ -5335,6 +5321,20 @@ const make__ASTAnonWildcardAlternative = arr => {
       ...arr[3]
     ]);
 }
+// -------------------------------------------------------------------------------------------------
+// A1111-style LoRAs:
+// -------------------------------------------------------------------------------------------------
+const A1111StyleLoraWeight = choice(/\d*\.\d+/, /\d+/);
+const A1111StyleLora       = xform(arr => new ASTLora(arr[3], arr[4][0]),
+                                   wst_seq('<',                                    // [0]
+                                           'lora',                                 // [1]
+                                           ':',                                    // [2]
+                                           choice(filename, () => LimitedContent), // [3]
+                                           optional(second(wst_seq(':',
+                                                                   choice(A1111StyleLoraWeight,
+                                                                          () => LimitedContent))),
+                                                    "1.0"), // [4][0]
+                                           '>'));
 // -------------------------------------------------------------------------------------------------
 // flag-related non-terminals:
 // -------------------------------------------------------------------------------------------------
