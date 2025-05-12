ac3a42f3ca63e7126263d8794d60c9c4345c1017
Ariane Emory <ariane.emory@protonmail.com>
2025-05-12 14:33:31 -0400

    fixed

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -68,7 +68,7 @@ function post_prompt(prompt, config = {}, { hostname = '127.0.0.1', port = 7860,
   console.log(`POSTing with config: ${JSON.stringify(config)}`);
 
   let data = { prompt: prompt, ...config,
-               negativePrompt: negative_prompt || negative_prompt === '' ? negative_prompt : undefined };
+               negative_prompt: negative_prompt || negative_prompt === '' ? negative_prompt : undefined };
 
   // doing this seems convenient?
   if (data.n_iter && (typeof data.n_iter === 'number') && data.n_iter > 1) { 
@@ -7129,6 +7129,8 @@ async function main() {
             [ negative_prompt, prior_negative_prompt ] = [ prior_negative_prompt, negative_prompt ]
             
             post_prompt(expanded, config, { negative_prompt: negative_prompt });
+
+            continue;
           }
           else {
             console.log(`can't rewind, no prior prompt`);
