cbe19ab5bc6efcef7d3ba96b0bb524390e634523
Ariane Emory <ariane.emory@protonmail.com>
2025-05-14 17:39:59 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -6916,16 +6916,24 @@ const SetFlag    = xform(second(seq('#', plus(ident, '.'), word_break)),
                                            `${inspect_fun(arr)}`);
                            return new ASTSetFlag(arr);
                          });
-const CheckFlagWithOrAlternatives= xform(second(seq('?', plus(plus(ident, '.'), ','), word_break)),
-                                         arr => {
-                                           if (log_flags_enabled)
-                                             if (arr.some(e => e.length > 1))
-                                               console.log(`CONSTRUCTING CHECKFLAG WITH ` +
-                                                           `${inspect_fun(arr)}`);
-                                           return new ASTCheckFlags(arr);
-                                         });
+const CheckFlagWithOrAlternatives = xform(second(seq('?', plus(plus(ident, '.'), ','), word_break)),
+                                          arr => {
+                                            if (log_flags_enabled)
+                                              if (arr.some(e => e.length > 1))
+                                                console.log(`CONSTRUCTING CHECKFLAG (1) WITH ` +
+                                                            `${inspect_fun(arr)}`);
+                                            return new ASTCheckFlags(arr);
+                                          });
+const CheckFlagWithSetConsequent  = xform(second(seq('?', plus(plus(ident, '.'), ','), word_break)),
+                                          arr => {
+                                            if (log_flags_enabled)
+                                              if (arr.some(e => e.length > 1))
+                                                console.log(`CONSTRUCTING CHECKFLAG (2) WITH ` +
+                                                            `${inspect_fun(arr)}`);
+                                            return new ASTCheckFlags(arr);
+                                          });
 
-const CheckFlag  = choice(CheckFlagWithOrAlternatives);
+const CheckFlag  = choice(CheckFlagWithOrAlternatives, CheckFlagWithSetConsequent);
 const NotFlag    = xform(seq('!', optional('#'), plus(ident, '.'), word_break),
                          arr => {
                            if (log_flags_enabled)
