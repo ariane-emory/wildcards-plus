6adf0e8061d000146504659e14950101fd0b9656
Ariane Emory <ariane.emory@protonmail.com>
2025-05-14 21:43:42 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -2800,13 +2800,28 @@ class Context {
 
     // skip already set flags:
     if (this.flags.some(existing_flag => arr_is_prefix_of_arr(new_flag, existing_flag))) {
-      if (log_flags_enabled)
-        console.log(`SKIPPING, ALREADY SET`);
+      // if (log_flags_enabled)
+      //   console.log(`SKIPPING, ALREADY SET`);
 
       return;
     }
+
+    const new_flag_head = new_flag.slice(0, -1);
     
-    this.flags = this.flags.filter(existing_flag => !(arr_is_prefix_of_arr(existing_flag, new_flag)));
+    this.flags = this.flags.filter(existing_flag => {
+      if (arr_is_prefix_of_arr(existing_flag, new_flag)) {
+        // console.log(`DISCARD ${existing_flag} BECAUSE 1`);
+        return false;
+      }
+      
+      if (new_flag_head.length != 0 && arr_is_prefix_of_arr(new_flag_head, existing_flag)) {
+        // console.log(`DISCARD ${existing_flag} BECAUSE 2`);
+        return false; 
+      }
+      
+      return true;
+    });
+
     this.flags.push(new_flag);
   }
   // -----------------------------------------------------------------------------------------------
