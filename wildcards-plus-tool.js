69d678c6301f7de8676bb5361bf8b185a0895d77
Ariane Emory <ariane.emory@protonmail.com>
2025-05-16 16:58:23 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -7177,7 +7177,7 @@ const ScalarAssignment        = xform(arr => {
   // if ( arr[0][1] == '+=')
   //   throw new Error("bomb");
   
-  return new ASTScalarAssignment(arr[0][0], arr[1], arr[0][1] == '+=');
+  return new ASTScalarAssignment(arr[0][0], arr[1], arr[0][1]);
 },
                                       wst_cutting_seq(wst_seq(ScalarDesignator,        // [0][0]
                                                               choice(incr_assignment_operator, assignment_operator)),    // -
