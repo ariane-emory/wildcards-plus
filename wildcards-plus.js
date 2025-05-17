8fa4603ee5969dc4db6d7874d3c5c06291bf76bd
Ariane Emory <ariane.emory@protonmail.com>
2025-05-17 00:10:32 -0400

    ...

--- wildcards-plus.js
+++ wildcards-plus.js
@@ -6998,7 +6998,7 @@ const pipeline_configuration      = clone_fun(pipeline.configuration);
 const ui_prompt                   = pipeline.prompts.prompt;
 const ui_hint                     = "";
 let   prompt_string               = ui_prompt;
-const default_batch_count         = 5;
+const default_batch_count         = 125;
 // -------------------------------------------------------------------------------------------------
 
 
