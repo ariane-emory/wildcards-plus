103064fa8e406e410bef1ea49259e849383d6704
Ariane Emory <ariane.emory@protonmail.com>
2025-05-19 19:45:58 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -2671,7 +2671,7 @@ const config_key_names = [
   { dt_name: 'zeroNegativePrompt',                automatic1111_name: 'zero_negative_prompt'                       },
 ];
 // -------------------------------------------------------------------------------------------------
-function get_alternate_name(find_value, find_key, return_key) {
+function get_alternate_name(return_key, find_value, find_key) {
   if (log_name_lookups_enabled)
     console.log(`\nLOOKING UP ${return_key} FOR ${inspect_fun(find_key)} ` +
                 `${inspect_fun(find_value)}`);
@@ -2735,11 +2735,11 @@ function get_alternate_name(find_value, find_key, return_key) {
 }
 // -------------------------------------------------------------------------------------------------
 function get_automatic1111_name(name) {
-  return get_alternate_name(name, 'dt_name', 'automatic1111_name');
+  return get_alternate_name('automatic1111_name', name, 'dt_name');
 }
 // -------------------------------------------------------------------------------------------------
 function get_dt_name(name) {
-  return get_alternate_name(name, 'automatic1111_name', 'dt_name');
+  return get_alternate_name('dt_name', name, 'automatic1111_name');
 }
 // -------------------------------------------------------------------------------------------------
 function get_our_name(name) {
