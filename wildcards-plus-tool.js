bdd74758aa7ee1a4aba5ecd0499adf395246a362
Ariane Emory <ariane.emory@protonmail.com>
2025-05-19 16:37:46 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -2729,8 +2729,8 @@ function get_automatic1111_name(name) {
 }
 // -------------------------------------------------------------------------------------------------
 function get_dt_name(name) {
-  console.log(`\nLOOKING UP DT_NAME FOR '${name}'`);
-  
+  console.log(`\CASE-CORRECTING A1111 NAME '${name}'`);
+
   let   name_lc = name.toLowerCase();
   const got_lc  = config_key_names.find(([dt_name, automatic1111_name]) =>
     automatic1111_name.toLowerCase() === name_lc);
