a67effaed185b5cd3cdbfea5d80c328b90f7b7d2
Ariane Emory <ariane.emory@protonmail.com>
2025-05-21 21:05:13 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -6888,15 +6888,15 @@ function expand_wildcards(thing, context = new Context(), indent = 0) {
   // log(log_expand_and_walk_enabled,
   //     `expand thing: ${abbreviate(Array.isArray(thing) ? thing.join(' ') : thing.toString())}`);
 
-  const str = abbreviate(Array.isArray(thing)
-                         ? thing.join(' ')
-                         : (typeof thing === 'string'
-                            ? inspect_fun(thing)
-                            : thing.toString()));
+  const thing_str = abbreviate(Array.isArray(thing)
+                               ? thing.join(' ')
+                               : (typeof thing === 'string'
+                                  ? inspect_fun(thing)
+                                  : thing.toString()));
   
   log(log_expand_and_walk_enabled,
       `Expanding wildcards in ${thing.constructor.name} ` +
-      `${str} ` + 
+      `${thing_str} ` + 
       `in ${context}` // +
       // `@ ${indent}`
      );
