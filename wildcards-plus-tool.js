32763ce6cc40d69ee6fcd1a831c2f9d88b20cf05
Ariane Emory <ariane.emory@protonmail.com>
2025-05-26 11:12:56 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -60,7 +60,7 @@ function parse_file(filename) {
   const prompt_input = fs.readFileSync(filename, 'utf8');
   const cache        = new Map();
   const old_log_match_enabled = log_match_enabled;
-  log_match_enabled  = false;
+  log_match_enabled  = true;
   const result       = Prompt.match(prompt_input, 0, 0, cache);
   log_match_enabled  = old_log_match_enabled;
   
@@ -925,7 +925,7 @@ class Element extends Rule {
           `${typeof ret === 'symbol'
              ? ret.toString()
              : abbreviate(compress(inspect_fun(ret)))} ` +
-          `from \n` +
+          `from ` +
           `${abbreviate(compress(inspect_fun(rule_match_result.value)))} ` +
           ``
          );
