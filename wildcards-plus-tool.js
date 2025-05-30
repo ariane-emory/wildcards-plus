58f35053304bcee0c17184f260be8b0d9e8bf911
Ariane Emory <ariane.emory@protonmail.com>
2025-05-30 14:15:59 -0400

    whoops, fix

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -8406,12 +8406,12 @@ const structural_chars          = '{|}';
 const syntax_chars              = '@#$%';
 const comment_beginning         = String.raw`\/\/|\/\*`;
 const plain_text_head = (additional_excluded_chars = '') =>
-      String.raw`\\.|(?![\s${syntax_chars}${structural_chars}${additional_excluded_chars}]|${comment_beginning}\S)`;
+      String.raw`\\.|(?![\s${syntax_chars}${structural_chars}${additional_excluded_chars}]|${comment_beginning})\S)`;
 
 const plain_text                =
-      r_raw`(?:${plain_text_head('' )}\S)(?:\\.|(?![\s${structural_chars}]|${comment_beginning}})\S)*`;
+      r_raw`(?:${plain_text_head('' )}(?:\\.|(?![\s${structural_chars}]|${comment_beginning}})\S)*`;
 const plain_text_no_semis       =
-      r_raw`(?:${plain_text_head(':')}\S)(?:\\.|(?![\s${structural_chars};]|${comment_beginning}})\S)*`;
+      r_raw`(?:${plain_text_head(':')}(?:\\.|(?![\s${structural_chars};]|${comment_beginning}})\S)*`;
 
 
 const wb_uint                 = xform(r_raw`\d+(?=[\s|}])`, parseInt);
