e8d933c8a86b8ce281a6b516ed8c3d2576f47afa
Ariane Emory <ariane.emory@protonmail.com>
2025-05-29 22:40:36 -0400

    wb_uint

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -8375,9 +8375,7 @@ const filename                    = r(/[A-Za-z0-9 ._\-()]+/);
 const ident                       = xform(r(/[a-zA-Z_-][0-9a-zA-Z_-]*\b/),
                                           str => str.toLowerCase().replace(/-/g, '_'));
 
-const plain_text                  = r(new RegExp(String.raw`(?:\\.|(?![@#$%{|}\s` +
-                                                 // structural_chars + 
-                                                 String.raw`;]|\/\/|\/\*)\S)+`));
+const plain_text                  = r(/(?:\\.|(?![@#$%{|}\s;]|\/\/|\/\*)\S)+/);
 const wb_uint                     = xform(new RegExp(String.raw`\d+(?=[\s|}])`),
                                           parseInt);
 any_assignment_operator           .abbreviate_str_repr('any_assignment_operator');
