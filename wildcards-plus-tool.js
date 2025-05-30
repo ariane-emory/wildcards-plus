ac9e3d830303ab53f20eee8e196ce52d3d31319b
Ariane Emory <ariane.emory@protonmail.com>
2025-05-30 19:18:09 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -8402,9 +8402,10 @@ const structural_word_break   = r(/(?=[\s|}])/);
 const with_swb                = rule =>
       first(seq(rule, structural_word_break));
 const seq_with_swb            = (...rules) =>
-      xform(seq(...rules, structural_word_break),
-            arr => arr.slice(0, -1));
-// cuts after ALL rules if a SWB isn't found, NOT after first rule:
+      first(seq(seq(...rules), structural_word_break));
+// these cut after ALL rules if a SWB isn't found, NOT after first rule:
+const cutting_with_swb                = rule =>
+      first(cutting_seq(rule, structural_word_break));
 const cutting_seq_with_swb    = (...rules) => 
       first(cutting_seq(seq(...rules), structural_word_break));
 // -------------------------------------------------------------------------------------------------
