849a95a73dfc77f638489562cac4cbede452ecd4
Ariane Emory <ariane.emory@protonmail.com>
2025-06-01 02:35:26 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -8715,14 +8715,13 @@ A1111StyleLora      .abbreviate_str_repr('A1111StyleLora');
 // =================================================================================================
 const rJsonc_internal_word_break = r(/(?=[\s,])/);
 const mod_rJsonc_external        = second(wst_seq(jsonc_comments,
-                                                  choice(first(seq(choice(() => mod_rJsoncObject,
+                                                  first(choice(seq(choice(() => mod_rJsoncObject,
                                                                           () => mod_rJsoncArray,
                                                                           rjsonc_string),
-                                                                   optional(() => SpecialFunctionTail))),
-                                                         first(seq(choice(json_null,     json_true,
+                                                                   optional(() => SpecialFunctionTail)),
+                                                               seq(choice(json_null,     json_true,
                                                                           json_false,    json_number),
-                                                                   () => SpecialFunctionTail,
-                                                                  ))),
+                                                                   () => SpecialFunctionTail))),
                                                   jsonc_comments));
 const mod_rJsonc_internal        = second(wst_seq(jsonc_comments,
                                                   choice(() => mod_rJsoncObject,
