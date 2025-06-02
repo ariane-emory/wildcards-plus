4e251eecf975a4e6e5e14aa5e168bc86cc12c466
Ariane Emory <ariane.emory@protonmail.com>
2025-06-02 13:51:38 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -3825,7 +3825,7 @@ const configuration_key_names = [
     shorthands: [ 'guidance',                                                                                    ] },
   { dt_name: 'guidingFrameNoise',                 automatic1111_name: 'cond_aug'                                   },
   { dt_name: 'hiresFix',                          automatic1111_name: 'high_resolution_fix',
-    shorthands: [ 'enable_hr',                                                                                   ] },
+    shorthands: [ 'enable_hr', 'hrf' ]                                                                             },
   { dt_name: 'hiresFixHeight',                    automatic1111_name: 'hires_first_pass_height_explanation',
     shorthands: [ 'firstphase_height',                                                                           ] },
   { dt_name: 'hiresFixStrength',                  automatic1111_name: 'hires_second_pass_strength_detail'          },
