eb705731f8b80c1e49d475ab60da59a59b00dea1
Ariane Emory <ariane.emory@protonmail.com>
2025-06-05 18:17:05 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -9875,7 +9875,6 @@ const structural_word_break   = r(/(?=[\s|}]|$)/)
       .abbreviate_str_repr('structural_word_break');
 // -------------------------------------------------------------------------------------------------
 const with_swb                = rule => first(seq(rule, structural_word_break));
-const seq_with_swb            = (...rules) => first(seq(seq(...rules), structural_word_break));
 // these cut after ALL rules if a SWB isn't found, NOT after first rule:
 const cutting_seq_with_swb    = (...rules) => first(cutting_seq(seq(...rules),
                                                                 structural_word_break));
@@ -9981,17 +9980,17 @@ const flag_ident = xform(seq(choice(ident, '*'),
                            return ret;
                          });
 const SimpleCheckFlag =
-      xform(seq_with_swb(question,
-                         flag_ident),
+      xform(with_swb(seq(question,
+                         flag_ident)),
             arr => {
               const args = [arr[1]];
               return new ASTCheckFlags(args);
             })
       .abbreviate_str_repr('SimpleCheckFlag');
 const SimpleNotFlag =
-      xform(seq_with_swb(bang,
+      xform(with_swb(seq(bang,
                          optional(hash),
-                         flag_ident),
+                         flag_ident)),
             arr => {
               const args = [arr[2],
                             { set_immediately: !!arr[1]}];
