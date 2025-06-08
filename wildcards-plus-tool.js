c2905f54bba9d4b3b3f9237ef7719d134da7b121
Ariane Emory <ariane.emory@protonmail.com>
2025-06-08 19:48:22 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -8918,10 +8918,9 @@ function expand_wildcards(thing, context = new Context(), { correct_articles = t
         
         context.named_wildcards.set(thing.name, got.original_value);
         
-        lm.indent(() => {
-          if (context.noisy)
-            lm.log(() => `unlatched ${thing.name} back to ${thing_str_repr(got.original_value)}`);
-        });
+        if (context.noisy)
+          lm.indent(() => lm.log(() => `unlatched ${thing.name} back to ` +
+                                 `${thing_str_repr(got.original_value)}`));
 
         throw new ThrownReturn(''); // produce no text.
       } 
