ecb8ae74800db3e9afed5f8ed09a9f9f0a522065
Ariane Emory <ariane.emory@protonmail.com>
2025-06-10 10:41:51 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -4145,74 +4145,74 @@ const configuration_key_names = [
     shorthands: [ "znp" ] },
 ];
 // -------------------------------------------------------------------------------------------------
-function get_other_name(return_key, find_key, find_value) {
+function get_entry(needle_key, alternate_key, needle_value) {
   if (log_name_lookups_enabled)
-    lm.log(`\nLOOKING UP ${return_key} FOR ` +
-           `${inspect_fun(find_key)} ` +
-           `${inspect_fun(find_value)}`);
+    lm.log(`\nLOOKING UP ${alternate_key} FOR ` +
+           `${inspect_fun(needle_key)} ` +
+           `${inspect_fun(needle_value)}`);
 
-  let find_value_lc = find_value.toLowerCase();
+  let needle_value_lc = needle_value.toLowerCase();
 
   // -----------------------------------------------------------------------------------------------
-  // is find_value a shorthand?
+  // is needle_value a shorthand?
   // -----------------------------------------------------------------------------------------------
-  let got     = configuration_key_names.find(obj => 
-    obj?.shorthands?.includes(find_value_lc))
+  let entry     = configuration_key_names.find(obj => 
+    obj?.shorthands?.includes(needle_value_lc))
 
-  if (got) {
+  if (entry) {
     if (log_name_lookups_enabled)
-      lm.log(`RETURN FROM SHORTHAND ${inspect_fun(got[return_key])}\n`);
+      lm.log(`RETURN FROM SHORTHAND ${inspect_fun(entry[alternate_key])}\n`);
 
-    return got[return_key];
+    return entry;
   }
 
   // -----------------------------------------------------------------------------------------------
   // is it just miscapitalized?
   // -----------------------------------------------------------------------------------------------
-  got = configuration_key_names.find(obj => {
+  entry = configuration_key_names.find(obj => {
     if (log_name_lookups_enabled)
-      lm.log(`test ${inspect_fun(obj[return_key].toLowerCase())} === ` +
-             `${inspect_fun(find_value_lc)} = ` +
-             `${obj[return_key].toLowerCase() === find_value_lc}`);
-    return obj[return_key].toLowerCase() === find_value_lc;
+      lm.log(`test ${inspect_fun(obj[alternate_key].toLowerCase())} === ` +
+             `${inspect_fun(needle_value_lc)} = ` +
+             `${obj[alternate_key].toLowerCase() === needle_value_lc}`);
+    return obj[alternate_key].toLowerCase() === needle_value_lc;
   });
 
-  if (got) {
+  if (entry) {
     if (log_name_lookups_enabled)
-      lm.log(`RETURNING CASE-CORRECTED ${return_key} ${inspect_fun(got[return_key])}\n`);
+      lm.log(`RETURNING CASE-CORRECTED ${alternate_key} ${inspect_fun(entry[alternate_key])}\n`);
     
-    return got[return_key];
+    return entry;
   } 
 
   // -----------------------------------------------------------------------------------------------
   // look up the alternate key:
   // -----------------------------------------------------------------------------------------------
-  got = configuration_key_names.find(obj => obj[find_key].toLowerCase() === find_value_lc);
+  entry = configuration_key_names.find(obj => obj[needle_key].toLowerCase() === needle_value_lc);
 
-  if (got) {
+  if (entry) {
     if (log_name_lookups_enabled)
-      lm.log(`GOT ${return_key} FOR ` +
-             `${inspect_fun(find_key)} ${inspect_fun(find_value)}`);
+      lm.log(`ENTRY ${alternate_key} FOR ` +
+             `${inspect_fun(needle_key)} ${inspect_fun(needle_value)}`);
     
-    return got[return_key];
+    return entry;
   }
 
   // -----------------------------------------------------------------------------------------------
   // didn't find it on either side, just return the argument:
   // -----------------------------------------------------------------------------------------------
   if (log_name_lookups_enabled) 
-    lm.log(`RETURNING ARGUMENT ${inspect_fun(find_value)}\n`);
+    lm.log(`RETURNING ARGUMENT ${inspect_fun(needle_value)}\n`);
 
   // possibly an error? maybe not always.
-  return find_value;
+  return null;
 }
 // -------------------------------------------------------------------------------------------------
 function get_dt_name(name) {
-  return get_other_name('dt_name',            'automatic1111_name', name);
+  return get_entry('automatic1111_name', 'dt_name', name)['automatic1111_name'];
 }
 // -------------------------------------------------------------------------------------------------
 function get_automatic1111_name(name) {
-  return get_other_name('automatic1111_name', 'dt_name',            name);
+  return get_entry('dt_name', 'automatic1111_name', name)['dt_name'];
 }
 // -------------------------------------------------------------------------------------------------
 function get_our_name(name) {
