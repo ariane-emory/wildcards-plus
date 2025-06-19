82546c0188b2b8ff363a2888e73c15c83ee18835
Ariane Emory <ariane.emory@protonmail.com>
2025-06-19 15:01:49 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -10300,15 +10300,17 @@ function audit_semantics(root_ast_node,
           
           if (log_level__audit >= 1)
             lm.log(`NO_ERRORS PASS (legal):`);
-          lm.indent(() =>
-            walk(currently_legal_options,
-                 local_context,
-                 // switch to no_errors mode: some things that would look sus during this pass might 
-                 // not look sus afterwards, f.e. { ?foo whatever | #foo }.
-                 audit_semantics_modes.no_errors, 
-                 true, // or maybe false? nah, i think this is corect... any children could also
-                 // get evaluated twice and so should be juded as_if_parralel, right?
-                 visited_copy));
+          lm.indent(() => {
+            for (const option of currently_legal_options)
+              walk(option,
+                   local_context,
+                   // switch to no_errors mode: some things that would look sus during this pass might 
+                   // not look sus afterwards, f.e. { ?foo whatever | #foo }.
+                   audit_semantics_modes.no_errors, 
+                   true, // or maybe false? nah, i think this is corect... any children could also
+                   // get evaluated twice and so should be juded as_if_parralel, right?
+                   visited_copy);
+          });
 
           if (log_level__audit >= 1)
             lm.log(`${local_audit_semantics_mode.toUpperCase()} PASS:`);
