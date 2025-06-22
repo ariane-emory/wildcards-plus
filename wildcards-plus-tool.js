73ad466f78f791bb14c126560df1003b6bf24ecb
Ariane Emory <ariane.emory@protonmail.com>
2025-06-21 21:26:50 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -306,7 +306,7 @@ let log_flags_enabled                             = false;
 let log_match_enabled                             = false;
 let log_name_lookups_enabled                      = false;
 let log_picker_enabled                            = false;
-let log_level__audit                              = 2;
+let log_level__audit                              = 0;
 let log_level__expand_and_walk                    = 0;
 let log_level__process_named_wildcard_definitions = 0;
 let log_level__smart_join                         = 0;
@@ -10354,6 +10354,8 @@ function audit_semantics(root_ast_node,
           // }
         }
         else {
+          const visited_copy = new Set(visited);
+
           if (log_level__audit >= 1)
             lm.log(`${local_audit_semantics_mode.toUpperCase()} PASS TO CHECK SEMANTICS, MAY ` +
                    `REVISIT SOME LATER:`);
@@ -10363,8 +10365,8 @@ function audit_semantics(root_ast_node,
                    local_context.clone(),
                    local_audit_semantics_mode,
                    in_named_wildcard_reference,
-                   new Set(visited)); /* we'll need to revisit these some of these nodes in a non-cloned
-                                         context next, so we'll use a copy of visited.
+                   visited_copy); /* we'll need to revisit these some of these nodes in a non-cloned
+                                     context next, so we'll use a copy of visited.
                                       */
           });
 
