02569d61ff9e223499109e1aa344124227ec44a3
Ariane Emory <ariane.emory@protonmail.com>
2025-06-28 11:34:12 -0400

    ...

--- wildcards-plus-tool.js
+++ wildcards-plus-tool.js
@@ -63,7 +63,7 @@ function parse_file(filename) {
   const old_log_level__expand_and_walk = log_level__expand_and_walk;
   const old_log_level__smart_join      = log_level__smart_join;
 
-  // log_level__smart_join = 2;
+  log_level__smart_join = 2;
   
   //log_match_enabled          = true;
   // log_flags_enabled          = true;
@@ -312,8 +312,8 @@ let log_level__expand_and_walk                    = 0;
 let log_level__process_named_wildcard_definitions = 0;
 let log_level__smart_join                         = 0;
 let prelude_disabled                              = false;
-let print_ast_then_die                            = false;
-let print_ast_before_includes_enabled             = false;
+let print_ast_then_die                            = true;
+let print_ast_before_includes_enabled             = true;
 let print_ast_after_includes_enabled              = false;
 let print_ast_json_enabled                        = false;
 let print_packrat_cache_counts_enabled            = false;
